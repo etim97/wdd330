5aae68393368003837e9de306d9645725d5d9135
Etim 97 <Wisdomlds97@gmail.com>
2025-06-18 12:26:33 +0100

    wk 05

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import {  getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const localstore = getLocalStorage("so-cart") || []
+  localstore.push(product);
+  setLocalStorage("so-cart", localstore);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
